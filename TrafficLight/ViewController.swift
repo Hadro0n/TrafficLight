765902024f66f0083e08aefe6968e46e875e42dd
Hadro0n <v.w.yakunin@gmail.com>
2024-12-11 19:23:22 +0300

    Create TrafficLight

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  TrafficLight
-//
-//  Created by Владислав Якунин on 11.12.2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    let currentAlpha: CGFloat = 0.3
+    
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColor.alpha = currentAlpha
+        yellowColor.alpha = currentAlpha
+        greenColor.alpha = currentAlpha
+        
+        redColor.layer.cornerRadius = redColor.frame.width / 2
+        yellowColor.layer.cornerRadius = yellowColor.frame.width / 2
+        greenColor.layer.cornerRadius = greenColor.frame.width / 2
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redColor.alpha == yellowColor.alpha {
+            redColor.alpha = CGFloat(1.0)
+            greenColor.alpha = currentAlpha
+        }
+        else if redColor.alpha > currentAlpha && yellowColor.alpha == greenColor.alpha {
+            yellowColor.alpha = CGFloat(1.0)
+            redColor.alpha = currentAlpha
+        }
+        else if yellowColor.alpha > currentAlpha && greenColor.alpha == redColor.alpha {
+            greenColor.alpha = CGFloat(1.0)
+            yellowColor.alpha = currentAlpha
+        }
+    }
 }
-
